42c97528edb8b1d29b77b16248a97a65cc712fdc
titouv <titouan.verhille@gmail.com>
2022-01-12 22:41:52 +0100

    Added Sun/Moon DarkMode toggle animation

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,38 @@ function updateColor() {
 function setColorMode() {
   if (localStorage.theme == "light") {
     localStorage.theme = "dark";
+    animateBtnOn();
   } else {
     localStorage.theme = "light";
+    animateBtnOff();
   }
   updateColor();
+}
+
+function animateBtnOn() {
+  sun = document.getElementById("switch-icon-sun");
+  moon = document.getElementById("switch-icon-moon");
+  sun.classList.add("rotate-[359deg]");
+  sun.classList.remove("opacity-100");
+  sun.classList.add("opacity-0");
+  moon.classList.remove("opacity-0");
+  moon.classList.add("opacity-100");
+  moon.classList.remove("rotate-[359deg]");
+  moon.classList.add("rotate-0");
+  // ROTATE ICON WITH BOUNCE EFFECT
+  // FADE BOTH ICON
+}
+
+function animateBtnOff() {
+  sun = document.getElementById("switch-icon-sun");
+  moon = document.getElementById("switch-icon-moon");
+  sun.classList.remove("rotate-[359deg]");
+  sun.classList.add("opacity-100");
+  sun.classList.remove("opacity-0");
+  moon.classList.add("opacity-0");
+  moon.classList.remove("opacity-100");
+  moon.classList.add("rotate-[359deg]");
+  moon.classList.remove("rotate-0");
+  // ROTATE ICON WITH BOUNCE EFFECT
+  // FADE BOTH ICON
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{njk,html,js}"],
+  content: ["./src/**/*.{njk,html,js}", "./assets/**/*.js"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        transitionTimingFunction: {
+          bounce: "cubic-bezier(.17,.67,.37,1.17)",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
